2c488673ebf0fbdf5e704bcc41961d33c6153f29
Michele1221 <54606888+Michele1221@users.noreply.github.com>
2019-09-10 17:52:20 -0700

    robot square

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,38 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot freckles = new Robot ();
 
         // 3. Put the robot's pen down
-
+freckles.penDown();
 
         // 6. Make the robot move as fast as possible
-
+freckles.setSpeed(10);
 
         // 5. Do everything below here 4 times
-
-
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
-    }
+int sides=0;
+while (sides<70) {
+	freckles.move(250);
+	freckles.setRandomPenColor();
+	freckles.turn(80);
+	freckles.turn(65);
+	sides++;
+	
+	
+	
 }
+Robot Fred = new Robot ();
+ sides=0;
+while (sides<56) {
+	Fred.setSpeed(10);
+Fred.move(sides);
+Fred.turn(45);
+Fred.setRandomPenColor();
+
+}
+        //         2. Move your robot 200 pixels
+freckles.move(200);
+
+        //         4. Turn the robot 90 degrees to the right (90 degrees
+   freckles.turn(90);
+    } }
